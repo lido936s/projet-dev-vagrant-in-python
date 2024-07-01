7a5f7327f3c7dd0b8880acf2c1146664b6b44672
Ludo IT <59924579+lido936s@users.noreply.github.com>
2024-07-01 16:04:35 +0200

    v1.1

--- CreateDisk_DATA&OS.py
+++ CreateDisk_DATA&OS.py
@@ -3,13 +3,20 @@ import os
 # Chemin vers le dossier home
 home_dir = os.path.expanduser("~")
 
-# Chemin vers le répertoire DATA
-data_dir = os.path.join(home_dir, "DATA")
+# Chemin vers le répertoire machine
+machine_dir = os.path.join(home_dir, "machine")
 
-# Chemin vers le répertoire OS
-os_dir = os.path.join(home_dir, "OS")
+# Chemin vers le répertoire DATA dans le répertoire machine
+data_dir = os.path.join(machine_dir, "DATA")
 
-# Créer les répertoires s'ils n'existent pas
+# Chemin vers le répertoire OS dans le répertoire machine
+os_dir = os.path.join(machine_dir, "OS")
+
+# Créer le répertoire machine s'il n'existe pas
+if not os.path.exists(machine_dir):
+    os.mkdir(machine_dir)
+
+# Créer les répertoires DATA et OS dans le répertoire machine s'ils n'existent pas
 if not os.path.exists(data_dir):
     os.mkdir(data_dir)
 
